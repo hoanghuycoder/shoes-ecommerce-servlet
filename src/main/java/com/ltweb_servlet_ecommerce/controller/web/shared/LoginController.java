5a74c8e8cde12428acec32c6efcba9a1fe229eb4
Trần Võ Hoàng Huy <21130386@st.hcmuaf.edu.vn>
2024-04-06 18:23:38 +0700

    feat: Done change role feature

--- src/main/java/com/ltweb_servlet_ecommerce/controller/web/shared/LoginController.java
+++ src/main/java/com/ltweb_servlet_ecommerce/controller/web/shared/LoginController.java
@@ -53,7 +53,7 @@ public class LoginController extends HttpServlet {
                 tmpUser.setEmail(userModel.getEmail());
                 tmpUser = userService.findWithFilter(tmpUser);
                 if (tmpUser != null && BCrypt.verifyer().verify(userModel.getPassword().toCharArray(), tmpUser.getPassword()).verified) {
-                    SessionUtil.getInstance().putValue(req, "USER_MODEL", tmpUser);
+                    SessionUtil.getInstance().putValue(req, SystemConstant.USER_MODEL, tmpUser);
                     CartUtil.setCartFromSessionForUser(SessionUtil.getInstance(), req, orderDetailsService, cartService, tmpUser.getId());
                     UserModel updateUserLogged = new UserModel();
                     updateUserLogged.setLastLogged(new Timestamp(System.currentTimeMillis()));
