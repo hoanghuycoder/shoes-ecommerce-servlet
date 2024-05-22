340c3c3c28be60a1827afc1b75d3a562e6beed5b
VoPhiHoang <vophihoang252003@gmail.com>
2024-05-22 19:51:32 +0700

    feat: show orders for user

--- src/main/java/com/ltweb_servlet_ecommerce/controller/web/shared/OrderController.java
+++ src/main/java/com/ltweb_servlet_ecommerce/controller/web/shared/OrderController.java
@@ -4,10 +4,11 @@ import com.ltweb_servlet_ecommerce.model.AddressModel;
 import com.ltweb_servlet_ecommerce.model.OrderDetailsModel;
 import com.ltweb_servlet_ecommerce.model.OrderModel;
 import com.ltweb_servlet_ecommerce.model.ProductModel;
+import com.ltweb_servlet_ecommerce.paging.Pageble;
 import com.ltweb_servlet_ecommerce.service.*;
 import com.ltweb_servlet_ecommerce.utils.CartUtil;
 import com.ltweb_servlet_ecommerce.utils.NotifyUtil;
-import com.ltweb_servlet_ecommerce.utils.UrlUtil;
+import com.ltweb_servlet_ecommerce.utils.PagingUtil;
 
 import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
@@ -19,9 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-@WebServlet(urlPatterns = {"/order-details*","/order-details/*"})
+@WebServlet(urlPatterns = {"/orders"})
 public class OrderController extends HttpServlet {
     @Inject
     IOrderService orderService;
@@ -33,36 +33,58 @@ public class OrderController extends HttpServlet {
     IProductService productService;
     @Inject
     IProductSizeService productSizeService;
-    @Inject ISizeService sizeService;
+    @Inject
+    ISizeService sizeService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            // Set up notifications for the request
             NotifyUtil.setUp(req);
-            String slug = UrlUtil.getIdFromUrl(req, resp);
-            List<Object> params = new ArrayList<>();
-            params.add(slug);
-            Map<String,Object> result = orderService.findIdBySlug(params);
-            if (result.get("id")!=null) {
-                OrderModel order = orderService.findById((Long) result.get("id"));
-                req.setAttribute("ORDER_MODEL",order);
-                AddressModel addressModel = addressService.findById(order.getAddressId());
-                req.setAttribute("ADDRESS_MODEL",addressModel);
+
+            // Set up paging and sorting for the order list
+            Pageble orderPaging = PagingUtil.setUpPagingAndSort(req);
+
+            // Retrieve all orders with the specified paging and sorting
+            List<OrderModel> listOrder = orderService.findAll(orderPaging);
+
+            // Iterate over each order
+            for (int i = 0; i < listOrder.size(); i++) {
+                // Get the current order
+                OrderModel orderModel = listOrder.get(i);
+
+                // Find the address associated with the current order
+                AddressModel addressModel = addressService.findById(orderModel.getAddressId());
+
+                // Create a new OrderDetailsModel and set its orderId to the current order's id
                 OrderDetailsModel orderDetailsModel = new OrderDetailsModel();
-                orderDetailsModel.setOrderId(order.getId());
-                List<OrderDetailsModel> listOrderDetails = orderDetailsService.findAllWithFilter(orderDetailsModel,null);
+                orderDetailsModel.setOrderId(orderModel.getId());
+
+                // Find all order details associated with the current order
+                List<OrderDetailsModel> listOrderDetails = orderDetailsService.findAllWithFilter(orderDetailsModel, null);
+
+                // Create a new list to hold the products associated with the current order
                 List<ProductModel> listProduct = new ArrayList<>();
+
+                // Iterate over each order detail
                 for (OrderDetailsModel orderDetails : listOrderDetails) {
-                    new CartUtil().setListProductFromOrderDetails(listProduct,orderDetails,productSizeService,productService,sizeService);
+                    // Add the product associated with the current order detail to the product list
+                    new CartUtil().setListProductFromOrderDetails(listProduct, orderDetails, productSizeService, productService, sizeService);
                 }
-                req.setAttribute("LIST_PRODUCT",listProduct);
-                RequestDispatcher rd = req.getRequestDispatcher("/views/web/order.jsp");
-                rd.forward(req, resp);
-            } else {
-                resp.sendRedirect("/error/404");
+
+                // Set the address of the current order
+                orderModel.setAddressModel(addressModel);
+
+                // Set the product list of the current order
+                orderModel.setListProduct(listProduct);
+
+                // Update the current order in the order list
+                listOrder.set(i, orderModel);
             }
+            req.setAttribute("orders", listOrder);
+            RequestDispatcher rd = req.getRequestDispatcher("/views/web/dash-my-order.jsp");
+            rd.forward(req, resp);
         } catch (Exception e) {
-            System.out.println(e.toString());
             resp.sendRedirect("/home?message=error&toast=danger");
         }
     }
